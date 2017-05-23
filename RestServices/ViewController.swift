8bfc2f733124d49ed5383bac18ece62768aa8158
Elliot Palominos <epalominos@cl-it00157.local>
2017-05-23 16:24:29 -0400

    Initial Commit

---
+++ RestServices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestServices
+//
+//  Created by Elliot Palominos on 5/23/17.
+//  Copyright © 2017 Elliot Palominos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
